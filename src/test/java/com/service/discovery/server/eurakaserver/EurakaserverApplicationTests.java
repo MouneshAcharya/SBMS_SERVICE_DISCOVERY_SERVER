dc6b6c090d40359876577828879ee688bf2fc7e4
Mounesh Acharya <mounesh.acharya51@gmail.com>
2024-06-25 11:17:16 +0530

    eureka server setup

---
+++ src/test/java/com/service/discovery/server/eurakaserver/EurakaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.discovery.server.eurakaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
